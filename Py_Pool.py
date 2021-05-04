4ca24616acd44556745423d1ceb546a7c6fdba07
Rajendra Matcha <32381045+RajendraMatcha@users.noreply.github.com>
2021-05-04 11:56:07 +0530

    Create Py_Pool.py

---
+++ Py_Pool.py
@@ -0,0 +1,21 @@
+# Multiprocessing using 'Pool'
+
+import time
+import multiprocessing as mp
+
+def sum_square(number):
+  s = 0
+  for ii in range(number):
+    s += ii*ii
+  return s
+
+if __name__ == '__main__':
+  total_cores = mp.cpu_count()
+  p = mp.Pool(processes=total_cores)
+  
+  numbers = range(5)
+  result = p.map(sum_square, numbers)
+  
+  p.close()
+  p.join()
+  
